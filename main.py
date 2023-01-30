57c531542e3eca8fb2464052ea1d2cf397d60324
Elliot <captainexpo@gmail.com>
2023-01-30 15:59:41 -0500

    It works! Also added finetuner.jsonl for fine-tuning, still need to figure that out though.

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import openai
 from flask import Flask
 from slackeventsapi import SlackEventAdapter
 
-
 load_dotenv('.env')
 openai.api_key = os.getenv('OPENAI_TOKEN')
 slack_token = os.getenv('SLACK_TOKEN')
@@ -15,33 +14,38 @@ signing_secret = os.getenv('SIGNING_SECRET')
 app = Flask(__name__)
 client = slack_sdk.WebClient(token=slack_token)
 slack_event_adapter = SlackEventAdapter(signing_secret, '/slack/events', app)
+
+
 @slack_event_adapter.on('app_mention')
 def message(payload):
-    event = payload.get('event',{})
+
+    event = payload.get('event', {})
     channel = event.get('channel')
     uid = event.get('user')
     text = event.get('text')
-    generateAndPostMsg(text,uid,channel)
+    generateAndPostMsg(text, uid, channel)
+
 
 try:
-    response = client.chat_postMessage(
+    client.chat_postMessage(
         channel="#8-ball-reincarnated-testing",
         text="8-ball has risen :skull:"
     )
-    #print(response)
 except SlackApiError as e:
     print(e)
 
 
-def generateAndPostMsg(text,userid,channel):
+def generateAndPostMsg(text, userid, channel):
     response = openai.Completion.create(
-        model="text-davinci-001",
+        engine="text-davinci-003",
         prompt=text,
-        temperature=0.1,
-        max_tokens=100
+        max_tokens=500,
+        n=1,
+        stop=None,
+        temperature=0.5
     )
-    client.chat_postMessage(channel=channel, text=response)
-
+    result = response.choices[0].text
+    client.chat_postMessage(channel=channel, text=result)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False)
